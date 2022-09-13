ac7e9ff50588c8ec8d081337eff65948277046df
CasparCheng <caspar939747993@gmail.com>
2022-09-12 22:08:21 -0600

    Visual Effect for Bonus item
    Create the star effect for the bonus item to attract player to pickup.

--- Assets/Scripts/BonusBody.cs
+++ Assets/Scripts/BonusBody.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class BonusBody : MonoBehaviour
 {
+    public GameObject starEffect;
+
+    void Start()
+    {
+        Instantiate(starEffect, transform.position, Quaternion.identity);
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            Instantiate(starEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }            
     }
